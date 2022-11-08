29c413013510e4cef2887cd3cd44de5a9ecc6997
Juan Ignacio Lucero <106396380+Juani9@users.noreply.github.com>
2022-11-07 21:17:15 -0300

    agrego z-index en principal

---
+++ Proyecto Integrador/Vistas/js/swetalert.js
@@ -0,0 +1,11 @@
+Swal.fire({
+	title:"Bienvenidos a G4.0",
+	text:"Donde haya un viajero estamos nosotros",
+	timer:2000,
+	timerProgressBar: true,
+	icon:"info",
+	position: "center",
+	showConfirmButton: false,
+
+});
+
