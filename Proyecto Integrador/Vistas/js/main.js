e29dab1ceb348ec0dc8b23c343d782bc32dfc70e
Augusto Dizanzo <augustodizanzo@gmail.com>
2022-11-13 21:52:08 -0300

    .

--- Proyecto Integrador/Vistas/js/main.js
+++ Proyecto Integrador/Vistas/js/main.js
@@ -6,12 +6,3 @@ document.querySelector(".menu-btn").addEventListener("click", () => {
   ScrollReveal().reveal('.nuestros-productos', { delay: 500 });
   ScrollReveal().reveal('.nuestros-servicios', { delay: 500 });
   ScrollReveal().reveal('.nuestros-servicios-two', { delay: 500 });
-
-
-
-const btnSwitch = document.querySelector('#switch');
-
-btnSwitch.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    btnSwitch.classList.toggle('active');
-});
