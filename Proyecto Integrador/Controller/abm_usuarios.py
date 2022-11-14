b70132f642a4c2900833de4d08c139271d7314cc
Christopher Morales <cancheritos.me@gmail.com>
2022-11-13 22:00:04 -0300

    creando controlador

--- Proyecto Integrador/Controller/abm_usuarios.py
+++ Proyecto Integrador/Controller/abm_usuarios.py
@@ -1,13 +1,14 @@
 import mysql.connector
 
-def crear_usuario(dni, nombre, apellido, fecha_nac, email, contraseña):
+#Crear usuario, insertar datos en tabla
+def crear_usuario(dni, nombre, apellido, fecha_nac, email, contrasenia):
     try:
         connection=mysql.connector.connect(host='localhost',database='rent_cars',user='root',password='admin')
         cursor = connection.cursor()
-        mySql_insert_query = '''INSERT INTO Clientes (Dni, Nombre, Apellido, Fecha_nac, Email, Contraseña)
+        mySql_insert_query = '''INSERT INTO Clientes (Dni, Nombre, Apellido, Fecha_nac, Email, Contrasenia)
                                 VALUES (%s, %s, %s, %s, %s, %s) '''
 
-        record = (dni, nombre, apellido, fecha_nac, email, contraseña)
+        record = (dni, nombre, apellido, fecha_nac, email, contrasenia)
         cursor.execute(mySql_insert_query, record)
         connection.commit()
         print("Cliente creado correctamente")
@@ -33,6 +34,28 @@ def eliminar_usuario(dni):
 
     except mysql.connector.Error as error:
         print("Fallo al borrar cliente{}".format(error))
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection is closed")
+
+def cambiar_contraseña(dni):
+
+    try:
+        connection=mysql.connector.connect(host='localhost',database='rent_cars',user='root',password='admin')
+
+        mySql_insert_query = " UPDATE Clientes SET Contrasenia WHERE Dni = %s;"
+
+        
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query, dni)
+        connection.commit()
+        print(cursor.rowcount, "Contraseña modificada") 
+
+    except mysql.connector.Error as error:
+        print("Fallo al modificar contraseña {}".format(error))
 
     finally:
         if connection.is_connected():
--- Proyecto Integrador/Controller/controlador.py
+++ Proyecto Integrador/Controller/controlador.py
@@ -1,8 +1,9 @@
 class Cliente():
-    def __init__(self, dni, nombre, apellido, f_nac, email, contraseña):
+    def __init__(self, dni, nombre, apellido, f_nac, email, contrasenia):
         self.dni = dni
         self.nombre = nombre
         self.apellido = apellido
         self.f_nac = f_nac
         self.email = email
-        self.__contraseña__ = contraseña
+        self.__contrasenia__ = contrasenia
+
