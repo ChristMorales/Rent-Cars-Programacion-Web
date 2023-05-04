fc2c07a766eef9d9f5fe56841ebd4d04e689ccb0
cecilia.mndiaz@gmail.com <cecilia.mndiaz@gmail.com>
2023-05-04 13:21:01 -0300

    ruteo

--- Proyecto Integrador/Vistas/spa/src/app/app.module.ts
+++ Proyecto Integrador/Vistas/spa/src/app/app.module.ts
@@ -2,10 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { IniciarSesionComponent } from './pages/iniciar-sesion/iniciar-sesion.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { NosotrosComponent } from './pages/nosotros/nosotros.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { InformacionComponent } from './shared/informacion/informacion.component';
+import { SucursalesComponent } from './shared/sucursales/sucursales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IniciarSesionComponent
+    PrincipalComponent,
+    NosotrosComponent,
+    ContactoComponent,
+    RegistroComponent,
+    InformacionComponent,
+    SucursalesComponent,
+
+
+
+
+
+
+
   ],
   imports: [
     BrowserModule
@@ -14,3 +35,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
