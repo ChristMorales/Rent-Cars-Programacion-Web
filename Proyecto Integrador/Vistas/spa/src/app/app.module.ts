568c30b4a1bce9ceec184f1eaf05c50e1cc0a14b
Augusto Dizanzo <augustodizanzo@gmail.com>
2023-05-05 10:55:57 -0300

    prueba routing

--- Proyecto Integrador/Vistas/spa/src/app/app.module.ts
+++ Proyecto Integrador/Vistas/spa/src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { LayoutModule } from "./layout/layout.module";
+import { PagesModule } from "./pages/pages.module";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+
   imports: [
-    BrowserModule
+    BrowserModule,
+    LayoutModule,
+    PagesModule,
+  
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
+
+
+
 
