f61fb11425b3eb7e356a3e8a9a134986a1ce0576
Christopher Morales <cancheritos.me@gmail.com>
2023-06-13 18:23:23 -0300

    db json

--- Proyecto Integrador/Vistas/spa/src/app/auth/registrarse/registrarse.component.ts
+++ Proyecto Integrador/Vistas/spa/src/app/auth/registrarse/registrarse.component.ts
@@ -7,8 +7,8 @@ import { FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./registrarse.component.css']
 })
 export class RegistrarseComponent implements OnInit{
+  [x: string]: any;
   form: FormGroup;
-  http: any;
   constructor (public formBuilder: FormBuilder) {
     this.form= this.formBuilder.group(
       {
@@ -20,28 +20,36 @@ export class RegistrarseComponent implements OnInit{
         apellido: ['', []],
         fechaNacimiento: ['', []],
       }
-    )
-  }
-  ngOnInit(): void {
-    
-  }
-  onEnviar(event: Event){
+    )}
 
+    ngOnInit(): void {
+    
+    }
+    onEnviar(event: Event){
+      event.preventDefault;
+      if(this.form.valid){
+        alert("Enviar al servidor")
+      }else{
+        this.form.markAllAsTouched();
+      }
+
+  
+    }
+    enviarFormulario() {
+      let formData: any = new FormData();
+      formData.append('email', this.form.get('email')?.value);
+      formData.append('username', this.form.get('username')?.value);
+      formData.append('password', this.form.get('password')?.value);
+      formData.append('dni', this.form.get('dni')?.value);
+      formData.append('nombre', this.form.get('nombre')?.value);
+      formData.append('apellido', this.form.get('apellido')?.value);
+      formData.append('fechaNacimiento', this.form.get('fechaNacimiento')?.value);
+      this['http'].post('http://localhost:8000/api/auth/registro/', formData)
+        .subscribe({
+          next: (response: any) => console.log(response),
+          error: (error: any) => console.log(error),
+        })
+    }
   }
-  enviarFormulario() {
-    let formData: any = new FormData();
-    formData.append('email', this.form.get('email')?.value);
-    formData.append('username', this.form.get('username')?.value);
-    formData.append('password', this.form.get('password')?.value);
-    formData.append('dni', this.form.get('dni')?.value);
-    formData.append('nombre', this.form.get('nombre')?.value);
-    formData.append('apellido', this.form.get('apellido')?.value);
-    formData.append('fechaNacimiento', this.form.get('fechaNacimiento')?.value);
-    this.http
-      .post('http://localhost:8000/api/auth/registro/', formData)
-      .subscribe({
-        next: (response: any) => console.log(response),
-        error: (error: any) => console.log(error),
-      })
-  }
-}
+
+
