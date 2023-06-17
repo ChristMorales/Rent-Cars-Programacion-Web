00043a07366602a3da11bf5e0404990d9affd593
ADizanzo <augustodizanzo@gmail.com>
2023-06-16 22:45:26 -0300

    cambios en html y css

--- Proyecto Integrador/Vistas/spa/src/app/pages/productos/productos.component.ts
+++ Proyecto Integrador/Vistas/spa/src/app/pages/productos/productos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TestApiService } from 'src/app/servicios/test-api.service';
-import { Router } from '@angular/router';
+// import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-productos',
